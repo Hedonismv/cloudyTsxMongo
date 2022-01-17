4fa0c46592b08c69ea6775d9fa4ec1a087362373
Vitaly Klyhun <klygun90@gmail.com>
2022-01-17 18:24:36 +0300

    added the path "/api/auth" and express.json parsing

--- server/index.js
+++ server/index.js
@@ -6,15 +6,24 @@ const app = express()
 const PORT = config.get('serverPort')
 const mongoUrl = config.get('mongoUrl')
 
+const authRouter = require('./routes/auth.routes')
+
+//By default express cant parse the JSON data, then we need to add this
+app.use(express.json())
+
+app.use('/api/auth', authRouter)
+
 const start = async () => {
     try{
+        console.log(mongoUrl)
         await mongoose.connect(mongoUrl)
 
         app.listen(PORT, () => {
             console.log(`Server was started on port ${PORT}`)
         })
     }catch (e) {
-        
+        console.log(e)
+        process.exit(1)
     }
 }
 
