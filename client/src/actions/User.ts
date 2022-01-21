3073f91a7ce50cd1d10ee2e2668a14452ea25b71
Vitaly Klyhun <klygun90@gmail.com>
2022-01-21 18:14:05 +0300

    create login

--- client/src/actions/User.ts
+++ client/src/actions/User.ts
@@ -13,3 +13,19 @@ export const registration:Function = async (email:string, password:string) => {
         console.log(e)
     }
 }
+
+
+export const login:Function = (email:string, password:string) => {
+    // @ts-ignore
+    return async dispatch => {
+        try{
+            const response = await axios.post('/api/auth/login', {
+                email,
+                password
+            })
+            console.log(response.data)
+        }catch (e) {
+            console.log(e)
+        }
+    }
+}
--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -1,9 +1,19 @@
-import React, {FC} from 'react';
+import React, {FC, useState} from 'react';
+import Input from "../styledComponent/Input";
+import {login} from "../actions/User";
+import {useDispatch} from "react-redux";
 
 const Login:FC = () => {
+    const [email, setEmail] = useState<string>('')
+    const [password, setPassword] = useState<string>('')
+    const dispatch = useDispatch()
+
     return (
         <div>
-            <h1>Login</h1>
+            <div>Логин</div>
+            <Input type={'text'} func={(e:React.ChangeEvent<HTMLInputElement>) => setEmail(e.target.value)} value={email} placeholder={'Введите Email'}/>
+            <Input type={'password'} func={(e:React.ChangeEvent<HTMLInputElement>) => setPassword(e.target.value)} value={password} placeholder={'Введите Пароль'}/>
+            <button onClick={() => dispatch(login(email, password))}>Войти</button>
         </div>
     );
 };
