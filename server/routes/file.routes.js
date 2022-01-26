ec47750a6900a40f3bb564d68c9667874fa5a351
Vitaly Klyhun <klygun90@gmail.com>
2022-01-26 19:28:45 +0300

    add the file routes post-get

---
+++ server/routes/file.routes.js
@@ -0,0 +1,12 @@
+const Router = require('express')
+const router = new Router()
+const authMiddleware = require('../middleware/auth.middleware')
+const fileController = require('../controllers/fileController')
+
+router.post('', authMiddleware, fileController.createDir)
+router.get('', authMiddleware, fileController.fetchFiles)
+
+
+
+
+module.exports = router
