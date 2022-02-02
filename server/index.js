a4951ff5ff43316782eb72e50d1eb484a9441ed2
Vitaly Klyhun <klygun90@gmail.com>
2022-02-02 16:09:54 +0300

    add middleware express downloader

--- server/index.js
+++ server/index.js
@@ -5,15 +5,20 @@ const config = require('config')
 const app = express()
 const PORT = config.get('serverPort')
 const mongoUrl = config.get('mongoUrl')
+const fileUpload = require('express-fileupload')
 
 const authRouter = require('./routes/auth.routes')
 const fileRouter = require('./routes/file.routes')
 
+app.use(fileUpload({})) // write middleware before routes
+
 //By default express cant parse the JSON data, then we need to add this
 app.use(express.json())
 
+//routes
 app.use('/api/auth', authRouter)
 app.use('/api/files', fileRouter)
+
 
 const start = async () => {
     try{
